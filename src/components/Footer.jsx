7cbafbdbe2150c4168b727b01fdd649156ac4167
Naman Markhedkar <marknaman05@gmail.com>
2022-09-07 22:25:08 +0530

    My first react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright &#169; {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
